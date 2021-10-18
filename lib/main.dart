610e4e3e958d13e6cffd50b528d884cdb8177d2e
CMO <KS09wilnd8>
2021-10-18 15:44:13 +0200

    Refactored sources to meet better maintainability

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:whatswapp/src/whatswapp_browser.dart';
 
 void main() {
   runApp(WhatsWapp());
 }
 
+/// Main Widget for WhatsWapp
 class WhatsWapp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,40 +15,6 @@ class WhatsWapp extends StatelessWidget {
         primarySwatch: Colors.blueGrey,
       ),
       home: WhatsWappBrowser(),
-    );
-  }
-}
-
-class WhatsWappBrowser extends StatefulWidget {
-  WhatsWappBrowser({Key? key}) : super(key: key);
-
-  @override
-  _WhatsWebBrowserState createState() => _WhatsWebBrowserState();
-}
-
-class _WhatsWebBrowserState extends State<WhatsWappBrowser> {
-  @override
-  void initState() {
-    super.initState();
-    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      appBar: AppBar(
-        title: Text('WhatsWapp'),
-      ),
-      body: WebView(
-        initialUrl: 'https://web.whatsapp.com',
-        allowsInlineMediaPlayback: true,
-        javascriptMode: JavascriptMode.unrestricted,
-        onWebViewCreated: (WebViewController controller) {
-          // Do nothing for now
-        },
-        userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
-      )
     );
   }
 }
